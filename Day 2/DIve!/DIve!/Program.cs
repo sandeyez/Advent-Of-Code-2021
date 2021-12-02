19e05918ebdaca9e4e76625addec29f8d92e7b6b
Sander van Werkhooven <s.a.f.vanwerkhooven@students.uu.nl>
2021-12-02 10:07:33 +0100

    Day 2

---
+++ Day 2/DIve!/DIve!/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DIve_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\sande\OneDrive\Desktop\Projects\AoC2021\Day 2\DIve!\input.txt");
+            int depth = 0;
+            int hor = 0;
+            int aim = 0;
+
+            foreach(string line in lines)
+            {
+                string[] values = line.Split();
+                string direction = values[0];
+                int value = Int32.Parse(values[1]); 
+
+                switch (direction)
+                {
+                    case "forward":
+                        {
+                            hor += value;
+                            depth += aim * value;
+                            break;
+                        }
+                    case "down":
+                        {
+                            //depth += value;
+                            aim += value;
+                            break;
+                        }
+                    case "up":
+                        {
+                            //depth -= value;
+                            aim -= value;
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine(depth * hor);
+
+        }
+    }
+}

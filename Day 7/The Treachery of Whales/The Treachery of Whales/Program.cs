490c0fb74878ed3fcb20122660da9c9841ae716c
Sander van Werkhooven <s.a.f.vanwerkhooven@students.uu.nl>
2021-12-07 09:35:37 +0100

    Day 7

---
+++ Day 7/The Treachery of Whales/The Treachery of Whales/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace The_Treachery_of_Whales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\sande\OneDrive\Desktop\Projects\AoC2021\Day 7\The Treachery of Whales\input.txt");
+            int[] input = Array.ConvertAll(lines[0].Split(','), int.Parse);
+            int result = int.MaxValue;
+            for (int i = input.Min(); i <= input.Max(); i++)
+            {
+                int diff = input.Select(num => sumDiff(num, i)).Sum();
+                if (diff < result)
+                {
+                    result = diff;
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        static int sumDiff (int start, int end)
+        {
+            int sum = 0;
+            for (int i = Math.Min(start, end); i <= Math.Max(start, end); i++)
+                sum += i - start;
+            return (Math.Abs(sum));
+        }
+    }
+}
